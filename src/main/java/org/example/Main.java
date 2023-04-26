e789d19f1016aaa3ba75e3ec6b3547499b95d60a
yberber <yusufberber19@gmail.com>
2023-04-26 23:44:35 +0200

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String nAme =  "Yusuf" ;
+        System.out.println("Hello world!");
+         System.out.println("Your name is: " + nAme);
+    }
+}
